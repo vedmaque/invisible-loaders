960c03e6fe1ad492c1ef48a85232d4d2ff3a5d27
Artyom Mikhaylov <vedmaque@gmail.com>
2021-08-02 19:28:18 +0300

    add example

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="invisible-loaders-example">
+    <h1>Invisile Loaders</h1>
+    <div class="list">
+      <div class="loader-item" v-for="item in items" :key="item.id">
+        <div class="loader-item__descr">state: {{ item.state }}</div>
+        <div class="loader-item__icon" v-show="item.state === 'loading'">
+          <IconLoad />
+        </div>
+        <div class="loader-item__trigger">
+          <button @click="toggleLoading(item)">toggle</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import IconLoad from '@/components/IconLoad.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    IconLoad,
+  },
+  data: () => ({
+    items: [
+      { id: 1, state: 'idle' },
+      { id: 2, state: 'idle' },
+      { id: 3, state: 'idle' },
+      { id: 4, state: 'idle' },
+    ]
+  }),
+  methods: {
+    toggleLoading(item) {
+      item.state = item.state === 'idle' ? 'loading' : 'idle'
+    },
+  },
 }
 </script>
 
@@ -22,5 +47,50 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.preloader-arc {
+  fill: #989cb4;
+}
+.icon {
+  display: inline-flex;
+}
+.icon svg {
+  width: 24px;
+  height: 24px;
+  fill: currentColor;
+}
+.loader-item {
+  display: flex;
+  align-items: center;
+  padding: 8px 16px;
+}
+.loader-item + .loader-item {
+  border-top: 1px solid gray;
+}
+.loader-item__icon {
+  margin-left: 16px;
+  display: flex;
+}
+.loader-item__icon .icon {
+  animation: rotate 1s linear infinite;
+}
+.loader-item__trigger {
+  margin-left: auto;
+}
+.loader-item__trigger button {
+  margin: 0;
+  padding: 0;
+  background-color: white;
+  border: 1px solid blue;
+  border-radius: 4px;
+  padding: 8px;
+}
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
---
+++ src/components/IconLoad.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="icon">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <defs>
+        <linearGradient id="icon-load-paint-0" x1="50%" x2="50%" y1="15.637%" y2="100%">
+          <stop offset="0%" stop-color="rgba(256,256,256,0);" />
+          <stop offset="100%" stop-color="#FFF" />
+        </linearGradient>
+      </defs>
+      <g fill="none" fill-rule="evenodd">
+        <rect width="24" height="24" />
+        <path
+          stroke="url(#icon-load-paint-0)"
+          stroke-linecap="round"
+          stroke-width="2"
+          d="M10.054841,18 C7.24204197,17.3725682 5.48679509,14.6544677 6.1343974,11.9289498 C6.78199015,9.20347811 9.58720092,7.50273465 12.4,8.13022196"
+        />
+        <path
+          stroke="url(#icon-load-paint-0)"
+          stroke-linecap="round"
+          stroke-width="2"
+          d="M15.654841,16 C12.842042,15.3725682 11.0867951,12.6544677 11.7343974,9.92894979 C12.3819902,7.20347811 15.1872009,5.50273465 18,6.13022196"
+          transform="rotate(180 14.8 11)"
+        />
+      </g>
+    </svg>
+  </div>
+</template>
